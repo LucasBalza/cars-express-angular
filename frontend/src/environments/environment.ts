a6e7933f0367f249b3ead65d97bb7b9a0e48a743
LucasBalza <lucas.balza@hotmail.fr>
2025-04-10 12:17:30 +0200

    changes

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
-    apiUrl: process.env['API_URL'] || 'http://localhost:5000/api',
+    apiUrl: (window as any).__env?.API_URL || 'http://localhost:5000/api',
     amplifyConfig: {
         Auth: {
-            region: process.env['AWS_REGION'] || 'eu-west-1',
-            userPoolId: process.env['USER_POOL_ID'],
-            userPoolWebClientId: process.env['USER_POOL_WEB_CLIENT_ID']
+            region: (window as any).__env?.AWS_REGION || 'eu-west-1',
+            userPoolId: (window as any).__env?.USER_POOL_ID,
+            userPoolWebClientId: (window as any).__env?.USER_POOL_WEB_CLIENT_ID
         }
     }
 }; 
